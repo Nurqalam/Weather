1428827ee32b068a73194aea542f9c0037b31ac2
Nurqalam <tnurkalam@gmail.com>
2022-03-26 00:01:50 +0500

    updateInterface conditionId switch(systemIconNameString)

--- Weather/Controllers/MainViewController.swift
+++ Weather/Controllers/MainViewController.swift
@@ -14,25 +14,37 @@ class MainViewController: UIViewController {
     @IBOutlet weak var weatherIconImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var feelsLikeTempLabel: UILabel!
-    @IBOutlet weak var cityLabel: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapRounded()
-        networkWeatherManager.delegate = self
+        
+        networkWeatherManager.onCompletion = { [weak self] currentWeather in
+            guard let self = self else {return}
+            self.updateInterfaceWith(weather: currentWeather)
+        }
+        
+        networkWeatherManager.fetchingWeatherManager(withCity: "Aktau")
     }
 
+    
     @IBAction func searchButtonPressed(_ sender: UIButton) {
-        presentSearchAlertController(withTitle: title, message: nil, style: .alert) { (city) in
-            self.networkWeatherManager.fetchingWeatherManager(withCity: city) 
+        presentSearchAlertController(withTitle: title, message: nil, style: .alert) { [unowned self] (city) in
+            self.networkWeatherManager.fetchingWeatherManager(withCity: city)
+        }
+    }
+    
+    
+    func updateInterfaceWith(weather: CurrentWeather) {
+        DispatchQueue.main.async {
+            self.cityLabel.text = weather.cityName
+            self.temperatureLabel.text = weather.temperatureString
+            self.feelsLikeTempLabel.text = weather.feelsLikeTempString
+            self.weatherIconImageView.image = UIImage(systemName: weather.systemIconNameString)
         }
     }
 }
 
-extension MainViewController: NetworkWeatherManagerDelegate {
-    func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather) {
-        print(currentWeather.cityName)
-    }
-}
--- Weather/Models/NetworkWeatherManager.swift
+++ Weather/Models/NetworkWeatherManager.swift
@@ -7,22 +7,18 @@
 
 import Foundation
 
-protocol NetworkWeatherManagerDelegate: class {
-    func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather)
-}
-
 class NetworkWeatherManager {
     
-    weak var delegate: NetworkWeatherManagerDelegate?
+    var onCompletion: ((CurrentWeather) -> Void)?
     
     func fetchingWeatherManager(withCity city: String) {
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&apikey=13b34caec18b8fcda44b6477c16aca9f"
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&apikey=13b34caec18b8fcda44b6477c16aca9f&units=metric"
         guard let url = URL(string: urlString) else {return}
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { (data, response, error) in
             if let data = data {
                 if let currentWeather = self.parseJSON(withData: data) {
-                    self.delegate?.updateInterface(self, with: currentWeather)
+                    self.onCompletion?(currentWeather)
                 }
             }
         }
